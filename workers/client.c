7c4c8f96990e81a31992c7ed6b4b7c27465e3e63
blin04 <ivanbokor17@gmail.com>
2023-08-27 23:26:49 +0200

    clients are now able to find their znodes in zk tree

--- workers/client.c
+++ workers/client.c
@@ -16,6 +16,14 @@ char MY_IP[32] = "";                        // ip address of current program
 char LEADER_IP[32] = "";                    // ip address of cluster leader
 char CLUSTER[32] = "";                      // name of cluster to which current program is assigned 
 
+// size of eb cluster and nodes assigned to that cluster
+int EB_CNT = 3;
+char *EB_NODES[] = {"node2", "node4", "node5"};
+
+// size of mod cluster and nodes assigned to that cluster
+int MOD_CNT = 2;
+char *MOD_NODES[] = {"node3", "node6"};
+
 void setIPAdress() {
     /* funtion for setting clients IP address */
 
@@ -43,7 +51,7 @@ void setHostName() {
     }
 }
 
-bool checkCluster(zhandle_t* zh, char* client_name, char* path) {
+bool checkCluster(zhandle_t* zh, char* path) {
     /* checks if client is assigned to particular
         cluster identified with path */
 
@@ -55,9 +63,44 @@ bool checkCluster(zhandle_t* zh, char* client_name, char* path) {
     }
 
     for (int i = 0; i < children.count; i++) {
-        if (strcmp(client_name, children.data[i]) == 0) return true;
+        if (strcmp(HOSTNAME, children.data[i]) == 0) return true;
     }
     return false;
+}
+
+void createCluster(zhandle_t* zh, char* cluster_name) {
+    /* function that creates znodes for a given cluster */
+
+    int i, r;
+    char path[15];
+    char buff[50];
+    if (cluster_name == "eb") {
+
+        r = zoo_create(zh, "/eb", MY_IP, strlen(MY_IP), &ZOO_OPEN_ACL_UNSAFE, 
+            ZOO_PERSISTENT, buff, sizeof(buff));
+        if (r != ZOK) printf("ERROR (%d): can't create /eb \n", r);
+
+        for (i = 0; i < EB_CNT; i++) {
+            path[0] = '\0';
+            strcpy(path, "/eb/");
+            strcat(path, EB_NODES[i]);
+            r = zoo_create(zh, path, MY_IP, strlen(MY_IP), &ZOO_OPEN_ACL_UNSAFE, 
+                ZOO_PERSISTENT, buff, sizeof(buff));
+        }
+    }
+    else if (cluster_name = "mod") {
+        r = zoo_create(zh, "/mod", MY_IP, strlen(MY_IP), &ZOO_OPEN_ACL_UNSAFE, 
+            ZOO_PERSISTENT, buff, sizeof(buff));
+        if (r != ZOK) printf("ERROR (%d): can't create /mod \n", r);
+
+        for (i = 0; i < MOD_CNT; i++) {
+            path[0] = '\0';
+            strcpy(path, "/mod/");
+            strcat(path, MOD_NODES[i]);
+            r = zoo_create(zh, path, MY_IP, strlen(MY_IP), &ZOO_OPEN_ACL_UNSAFE, 
+                ZOO_PERSISTENT, buff, sizeof(buff));
+        }
+    }
 }
 
 int main() 
@@ -90,52 +133,38 @@ int main()
         strcpy(LEADER_IP, MY_IP);
 
         // creating /eb and client znodes (izdvojiti u posebnu funkciju)
-        r = zoo_create(zkHandler, "/eb", LEADER_IP, strlen(LEADER_IP),
-            &ZOO_OPEN_ACL_UNSAFE, ZOO_PERSISTENT, buff, sizeof(buff));
-        if (r != ZOK)
-            printf("ERROR! error code %d\n", r);
-
-        r = zoo_create(zkHandler, "/eb/node2", "", strlen(""), 
-            &ZOO_OPEN_ACL_UNSAFE, ZOO_PERSISTENT, buff, sizeof(buff));
-        if (r != ZOK)
-            printf("ERROR! error code %d\n", r);
-
+        createCluster(zkHandler, "eb");
         printf("Leader created /eb\n");
 
         // creating /mod and client znodes (izdvojiti u posebnu funkciju)
-        r = zoo_create(zkHandler, "/mod", LEADER_IP, strlen(LEADER_IP),
-            &ZOO_OPEN_ACL_UNSAFE, ZOO_PERSISTENT, buff, sizeof(buff));
-        if (r != ZOK)
-            printf("ERROR! error code %d\n", r);
-
-        r = zoo_create(zkHandler, "/mod/node3", "", strlen(""), 
-            &ZOO_OPEN_ACL_UNSAFE, ZOO_PERSISTENT, buff, sizeof(buff));
-        if (r != ZOK)
-            printf("ERROR (%d): can't create znode \n", r);
-        
+        createCluster(zkHandler, "mod");
         printf("Leader created /mod\n");
-
     }
     else {
         // this client failed to create /max znode, therfore it is a worker 
         
-        if (r != ZOK) {
-            // this client is in mod cluster
+        // check to which cluster does this worker belong
+        if (checkCluster(zkHandler, "/eb")) {
+            printf("%s is in /eb cluster\n", HOSTNAME);
 
-            if (checkCluster(zkHandler, HOSTNAME, "/eb")) {
-                printf("%s is in /eb cluster\n", HOSTNAME);
-            }
-            else if (checkCluster(zkHandler, HOSTNAME, "/mod")) {
-                printf("%s is in /mod cluster\n", HOSTNAME);
-            }
-            else {
-                printf("ERROR: no cluster found!\n");
-            }
-
-            int len = sizeof(LEADER_IP);
-            r = zoo_get(zkHandler, "/max", 0, LEADER_IP, &len, NULL);
-            if (r != ZOK) printf("ERROR (%d): can't get leader ip \n", r);
+            char path_to_node[15] = "/eb/";
+            strcat(path_to_node, HOSTNAME);
+            r = zoo_set(zkHandler, path_to_node, MY_IP, strlen(MY_IP), -1);
         }
+        else if (checkCluster(zkHandler, "/mod")) {
+            printf("%s is in /mod cluster\n", HOSTNAME);
+
+            char path_to_node[15] = "/mod/";
+            strcat(path_to_node, HOSTNAME);
+            r = zoo_set(zkHandler, path_to_node, MY_IP, strlen(MY_IP), -1);
+        }
+        else {
+            printf("ERROR: no cluster found!\n");
+        }
+
+        int len = sizeof(LEADER_IP);
+        r = zoo_get(zkHandler, "/max", 0, LEADER_IP, &len, NULL);
+        if (r != ZOK) printf("ERROR (%d): can't get leader ip \n", r);
     }
 
     printf("Leader is: %s\n", LEADER_IP);
