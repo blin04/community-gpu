bcfb132751c4432dea237e38ab20e3476d96ddc6
blin04 <ivanbokor17@gmail.com>
2023-09-20 23:01:37 +0200

    added naming of containers on cloud

--- config/start_container_cloud.sh
+++ config/start_container_cloud.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-docker run -it --rm -h node$1 -v /home/ivan/dist-community/config/zoo.cfg:/conf/zoo.cfg \
-	-v /home/ivan/dist-community/:/project -v /home/ivan/dist-community/config/hosts:/etc/hosts \
-	-p 6969:6969 us-central1-docker.pkg.dev/fast-ability-399210/petnica-docker-repo/zklibs /bin/bash
+docker run -d --rm --network=host -h node$1 --name node$1 -v /home/ivan/dist-community/config/zoo.cfg:/conf/zoo.cfg \
+        -v /home/ivan/dist-community/:/project -v /home/ivan/dist-community/config/hosts:/etc/hosts\
+        -v /home/ivan/dist-community/config/myid:/data/myid us-central1-docker.pkg.dev/fast-ability-399210/petnica-docker-repo/zklibs
+
