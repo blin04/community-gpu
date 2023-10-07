8b26013915f992603795d89d2f112def35fbbd04
blin04 <ivanbokor17@gmail.com>
2023-10-07 12:12:12 +0200

    sharing whole dataset folder with containers

--- config/start_container_cloud.sh
+++ config/start_container_cloud.sh
@@ -2,6 +2,6 @@
 
 docker run -d --rm --network=host -h node$1 --name node$1 -v /home/ivan/dist-community/config/zoo.cfg:/conf/zoo.cfg \
         -v /home/ivan/dist-community/system:/project -v /home/ivan/dist-community/config/hosts:/etc/hosts \
-        -v /home/ivan/dist-community/config/myid:/data/myid -v /home/ivan/dist-community/dataset/edges:/graph/edges \
-		-v /home/ivan/dist-community/dataset/nodes:/graph/nodes us-central1-docker.pkg.dev/fast-ability-399210/petnica-docker-repo/zklibs
+        -v /home/ivan/dist-community/config/myid:/data/myid -v /home/ivan/dist-community/dataset:/graph \
+		us-central1-docker.pkg.dev/fast-ability-399210/petnica-docker-repo/zklibs
 
